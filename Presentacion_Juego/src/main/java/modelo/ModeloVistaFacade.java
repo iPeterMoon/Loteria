c8ec8bdc0b03a456a43927977c17b4e03fdf7026
vzlaisa <147114185+vzlaisa@users.noreply.github.com>
2025-09-23 22:21:37 -0700

    Inicio de interfaces e implementaciones de modelo vista

---
+++ Presentacion_Juego/src/main/java/modelo/ModeloVistaFacade.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author rocha
+ */
+public class ModeloVistaFacade implements IModeloVista {
+    
+    @Override
+    public void colocarFicha(int numero) {
+        
+    }
+}
