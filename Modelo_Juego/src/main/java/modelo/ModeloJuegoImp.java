ef16b899d954308b0e707858357252cc7461269a
DeniseGarc <aliciadenise0623@gmail.com>
2025-09-29 17:48:31 -0700

    Descripción del flujo del metodo validarMovimiento
    Se agregó a las dependencias del proyecto modelo juego el mvc de juego.
    - Se agregó el flujo que debe de tener el metodo validarMovimiento del ModeloJuegoImp como documentación.

--- Modelo_Juego/src/main/java/modelo/ModeloJuegoImp.java
+++ Modelo_Juego/src/main/java/modelo/ModeloJuegoImp.java
@@ -12,9 +12,16 @@ import java.awt.Point;
  */
 public class ModeloJuegoImp implements IModeloJuego {
 
+    IModeloVista modeloVista = new ModeloVistaFacade();
+
     @Override
     public void validaMovimiento(Point posicion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       // Se debe de obtener la tarjeta del jugador
+       // obtener la carta en la posicion dada.
+       // verificar la carta actual
+       //si son iguales es decir la carta en la posicion actual y la carta cantada actual
+            // modeloVista.colocarFicha(posicion);
+       // en caso contrario no pasaria nada
     }
 
 }
