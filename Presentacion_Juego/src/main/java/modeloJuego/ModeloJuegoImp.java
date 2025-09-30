5aa953b92bc875c794c4557904e01542c4e385cb
DeniseGarc <aliciadenise0623@gmail.com>
2025-09-29 17:34:35 -0700

    Estructura de IModeloJuego y ModeloJUuegoImp
    - Se agrego el IModeloJuego.
    -Se agregó el ModeloJuegoImp.

---
+++ Presentacion_Juego/src/main/java/modeloJuego/ModeloJuegoImp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modeloJuego;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author Alici
+ */
+public class ModeloJuegoImp implements IModeloJuego {
+
+    @Override
+    public void validaMovimiento(Point posicion) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
