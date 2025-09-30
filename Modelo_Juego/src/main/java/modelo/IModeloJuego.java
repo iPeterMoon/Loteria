35d8042b379f8bb21dfb59fca44311370d05fcd6
DeniseGarc <aliciadenise0623@gmail.com>
2025-09-29 17:36:18 -0700

    Movimiento del modelo del juego
    Se movió el modelo del juego a un proyecto separado.

--- Modelo_Juego/src/main/java/modelo/IModeloJuego.java
+++ Modelo_Juego/src/main/java/modelo/IModeloJuego.java
@@ -1,9 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package modelo;
+
+import java.awt.Point;
 
 /**
  *
- * @author Pedro Luna Esquer
+ * @author Alici
  */
 public interface IModeloJuego {
-    public void colocarFicha();
+
+    public void validaMovimiento(Point posicion);
 }
