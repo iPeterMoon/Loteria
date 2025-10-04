207cb425c88bf0de61fde13bc7ef340912b7663d
rapayel <luisrafaellagarda@gmail.com>
2025-10-04 09:45:21 -0600

    correcciones de documentacion de Juego

--- Presentacion_Juego/src/main/java/control/ControlSeleccionarJugada.java
+++ Presentacion_Juego/src/main/java/control/ControlSeleccionarJugada.java
@@ -6,14 +6,17 @@ import modelo.ModeloControlImp;
 import modeloJuego.ModeloJuegoImp;
 
 /**
- *
+ * Clase Control que mantiene una conexion entre las clases
  * @author Jp
  */
 public class ControlSeleccionarJugada {
 
     // Método para inicializar el control
     private static IModeloControl controlModelo = new ModeloControlImp();
-
+    /**
+     * Metodo de control para colocar una ficha
+     * @param posicion posicion de la ficha
+     */
     public static void colocarFicha(Point posicion) {
         if (controlModelo != null) {
             controlModelo.colocarFicha(posicion);
