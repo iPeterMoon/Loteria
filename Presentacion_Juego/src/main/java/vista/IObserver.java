e089b98515c5a7f61b126b15c33c25b5e5a3e9e2
rapayel <luisrafaellagarda@gmail.com>
2025-10-04 07:55:00 -0600

    documentacion
    documentacion del la clase IObsever y FrameJuego

--- Presentacion_Juego/src/main/java/vista/IObserver.java
+++ Presentacion_Juego/src/main/java/vista/IObserver.java
@@ -7,10 +7,16 @@ package vista;
 import modelo.Subject;
 
 /**
- *
+ * clase interface del IObserver para definir 
+ * un contrato que las clases deben cumplir, 
+ * especificando qué métodos deben tener pero 
+ * no cómo se implementan
  * @author rocha
  */
 public interface IObserver {
-
+    /**
+     * metodo para actualizar el juego
+     * @param subject representacion del sujeto
+     */
     public void update(Subject subject);
 }
