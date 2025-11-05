437e59d11e4049ce5627b245c73fc66f83f385f3
Juamp-i5 <juan.olivarria252886@potros.itson.edu.mx>
2025-11-05 11:28:43 -0700

    estructura final del proyecto

---
+++ MatchMaker/ArrancadorMatchMaker/src/main/java/org/itson/arrancadormatchmaker/ArrancadorMatchMaker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.arrancadormatchmaker;
+
+/**
+ *
+ * @author Jp
+ */
+public class ArrancadorMatchMaker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ MatchMaker/MatchMakerImpl/src/main/java/org/itson/matchmakerimpl/MatchMakerImpl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.matchmakerimpl;
+
+/**
+ *
+ * @author Jp
+ */
+public class MatchMakerImpl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
